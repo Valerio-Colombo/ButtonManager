6647283bd953709dbaa5a5eb9f68f1cdfa44dd68
Valerio-Colombo <valecolomb96@gmail.com>
2019-02-02 02:13:49 +0100

    Added support for concatenated sliders

--- Slider.cpp
+++ Slider.cpp
@@ -11,7 +11,11 @@ Slider::Slider(int size, int digitalPin[]) {
 	}
 }
 
-Slider::Slider(int size, uint8_t* input) {
+Slider::Slider(int size, uint8_t* input, uint8_t* latchPin, uint8_t* dataPin, uint8_t* clockPin) {
+	_latchPin = latchPin;
+	_dataPin = dataPin;
+	_clockPin = clockPin;
+	_input = input;
 	_size = size;
 	_delta = 100/((size-1)*2);
 	_buttons = new Button *[size];
@@ -24,6 +28,69 @@ Slider::Slider(int size, uint8_t* input) {
 		size-=8;
 		input+=1;
 	}
+}
+
+void Slider::updateStatus() {
+	int iterations = (_size/8);
+
+	for(int i=0; i<=iterations; i++) {
+		//Pulse the latch pin:
+  		//set it to 1 to collect parallel data
+  		digitalWrite(*(_latchPin+i), 1);
+  		//set it to 1 to collect parallel data, wait
+  		digitalWrite(*(_clockPin+i), 1);
+  		delayMicroseconds(50);
+  		//set it to 0 to transmit data serially
+  		digitalWrite(*(_latchPin+i), 0);
+
+  		//while the shift register is in serial mode
+  		//collect each shift register into a byte
+  		//the register attached to the chip comes in first
+  		*_input = shiftIn(*_dataPin, *_clockPin);
+
+  		//Debuging print statements
+  		//Serial.print(*input, BIN);
+  		//Serial.print(" - ");
+  	}
+}
+
+uint8_t Slider::shiftIn(int myDataPin, int myClockPin) {
+	int i;
+  	int temp = 0;
+  	int pinState;
+  	byte myDataIn = 0;
+
+  	pinMode(myClockPin, OUTPUT);
+  	pinMode(myDataPin, INPUT);
+  	/*We will be holding the clock pin high 8 times (0,..,7) at the
+  	* end of each time through the for loop
+
+  	* at the begining of each loop when we set the clock low, it will
+  	* be doing the necessary low to high drop to cause the shift
+  	* register's DataPin to change state based on the value
+  	* of the next bit in its serial information flow.
+  	* The register transmits the information about the pins from pin 7 to pin 0
+  	* so that is why our function counts down
+  	*/
+  	for (i = 7; i >= 0; i--) {
+	    digitalWrite(myClockPin, 0);
+	    delayMicroseconds(0.2);
+	    temp = digitalRead(myDataPin);
+	    if (temp) {
+      		pinState = 1;
+      		//set the bit to 0 no matter what
+      		myDataIn = myDataIn | (1 << i);
+    	}
+    	else {
+      		pinState = 0;
+    	}
+
+    //Debuging print statements
+    //Serial.print(pinState);
+    digitalWrite(myClockPin, 1);
+
+  }
+  return myDataIn;
 }
 
 Transition Slider::getTransition() {
--- Slider.h
+++ Slider.h
@@ -8,14 +8,24 @@
 
 class Slider {
 	private:
-		int _size;
-		Button **_buttons;
-		float _delta;
+		// Initialization variables
+		int _size; // Size of the slider
+		uint8_t* _input; // Byte where slider reading are stored
+		Button **_buttons; // Slider buttons
+		float _delta; // Value quantum
 
+		// Pin variables
+		uint8_t* _latchPin;
+		uint8_t* _dataPin;
+		uint8_t* _clockPin;
+
+		// Measurements variables
 		bool _pressed = false;
 		float _progress = 0; // Actual value of the slider
-		int _holdCounter = 0;
+		int _holdCounter = 0; //Current time
 		int _holdThreshold = 150; // Number of milliseconds that must elapse to accept a change in the progress of the slider
+
+		uint8_t shiftIn(int myDataPin, int myClockPin);
 
 	public:
 		/*
@@ -24,7 +34,7 @@ class Slider {
  		*Use of the class is the same in both cases
  		*/
 		Slider(int size, int digitalPin[]);
-		Slider(int size, uint8_t* input);
+		Slider(int size, uint8_t* input, uint8_t* latchPin, uint8_t* dataPin, uint8_t* clockPin);
 
 		/*
  		*Return if the slider is:
@@ -42,14 +52,20 @@ class Slider {
 		State getState();
 
 		/*
-		Return curret value of slider if pressed or return last progress value if not pressed
+		*Return curret value of slider if pressed or return last progress value if not pressed
+		*If an invalid number of button is pressed the function returns -1
 		*/
 		int getProgress();
 
 		/*
-		Set _holdThresholdValue
+		*Set _holdThresholdValue
 		*/
 		int setHoldThreshold(int value);
+
+		/*
+		*Update slider status 
+		*/
+		void updateStatus();
 };
 
 #endif
