b07635740fff2f44980a319733183d094fa04531
Valerio-Colombo <valecolomb96@gmail.com>
2019-08-08 22:04:26 +0200

    Update example

--- Examples/SliderTest/SliderTest.ino
+++ Examples/SliderTest/SliderTest.ino
@@ -3,23 +3,17 @@
 #include <State.h>
 #include <Transition.h>
 
-#define BUTTON_SIZE 1
-#define SLIDER_SIZE 7
+#define SLIDER_SIZE 8
 
 // Sensor
-uint8_t ledPin = D2;                          // Led strip pin
-uint8_t latchPin = D7;                        // PSC
-uint8_t dataPin = D5;                         // Q6
-uint8_t clockPin = D6;                        // CLK
-uint8_t inputSlider = 0;                      // Byte where is stored 8-Stage Static Shift Register output
-Button buttonArray[BUTTON_SIZE] = {D0};       // Don't use D3, D4 and D8! They are reserved pin for boot
+uint8_t ledPin = 3;                          // Led strip pin
+uint8_t dataPin = 5;                         // Q8
+uint8_t clockPin = 6;                        // CLK
+uint8_t latchPin = 7;                        // PSC
+uint8_t inputSlider = 0;             // Byte where is stored 8-Stage Static Shift Register output
 Slider slider(SLIDER_SIZE, &inputSlider, latchPin, dataPin, clockPin);
 int progress = 0;
 int currentLuminosity = 0;
-
-void buttonArraySetup() {
-  buttonArray[0].setAction(roomLightButton);
-}
 
 void setup() {
   Serial.begin(9600);
@@ -28,18 +22,10 @@ void setup() {
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, INPUT);
 
-  buttonArraySetup();
+  slider.setHoldThreshold(100);
 }
 
 void loop() {
-  // Get buttons status
-  for (int i = 0; i < BUTTON_SIZE; i++) {
-    Transition transition = buttonArray[i].getTransition();
-    if (transition == Transition::RELEASED) {
-      buttonArray[i].doAction();
-    }
-  }
-
   // Get Slider status
   slider.updateStatus();
 
@@ -66,10 +52,5 @@ void setBedLight(int p) {
   if (currentLuminosity < p) currentLuminosity++;
   else if (currentLuminosity > p) currentLuminosity--;
   analogWrite(ledPin, map(currentLuminosity, 0, 100, 0 , 1024));
-}
-
-void roomLightButton() {
-  Serial.println(progress);
-  progress = 0;
 }
 
